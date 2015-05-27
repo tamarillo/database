016ae08e1f3d8732b08e8a5589cba2a9d49a35d6
Dengo <developer@dengophp.de>
2015-05-27 11:42:46 +0200

    Update Builder.php

--- Query/Builder.php
+++ Query/Builder.php
@@ -17,27 +17,38 @@ class Builder {
 	public $whereCons;
 
 	/**
+	 * @param \Tamarillo\Database\Connection $aConnection
+	 */
+	public function __construct($aConnection)
+	{
+		$this->DbConnection = $aConnection;
+	}
+
+	/**
 	 * Creates a new instance from self.
 	 *
 	 * @return \Tamarillo\Database\Query\Builder $builder
 	 */
 	public function createQuery()
 	{
-		return new static($this->connection);
+		return new static($this->DbConnection);
 	}
-	
+
 	/**
-	*
-	*
-	* @return \Tamarillo\Database\Query\Builder $this
-	*/
+	 * Standard where constraints.
+	 *
+	 * @param $column
+	 * @param null $operator
+	 * @param null $value
+	 *
+	 *  @return \Tamarillo\Database\Query\Builder $this
+	 */
 	public function where($column, $operator = null, $value = null)
 	{
-	  if (func_num_args() == 2)
-		{
-			list($value, $operator) = [$operator, '='];
-		}
-	  $this->whereCons[] = compact($column, $operator, $value);
+		if (func_num_args() == 2) list($value, $operator) = [$operator, '='];
+		$this->whereCons[] = compact($column, $operator, $value);
+
+		return $this;
 	}
 
 }
